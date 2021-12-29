ec97fab624face54430d40fc13c5e46314d81e14
egioacchini346 <eag346@drexel.edu>
2021-12-28 21:12:46 -0500

    updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,33 +8,38 @@ import { MorphSVGPlugin } from "gsap/MorphSVGPlugin";
 gsap.registerPlugin(GSDevTools, MotionPathPlugin, DrawSVGPlugin, MorphSVGPlugin);
 
 gsap.set("#ball",{ y:-600, opacity: 0, transformOrigin:"center"});
+gsap.set("#top-of-hat",{opacity: 0, transformOrigin:"center"});
+gsap.set("#bottom-of-hat",{opacity: 0, transformOrigin:"center"});
+gsap.set("#bottom-of-hat-stroke",{opacity: 0, transformOrigin:"center"});
 
 const mainTL = gsap.timeline() 
+
 
 function ball(){
     const tl=gsap.timeline();
     tl.to("#ball", {opacity: 1, duration: .30}, "same")
-    .to("#ball", {y:0, duration: .7}, "same")
+    tl.to("#ball", {y:0, duration: 1}, "same")
+    tl.to("#ball", {duration: 1, morphSVG:"#top-of-hat"})
 
-    CustomBounce.create("myBounce", {strength: 0.7, squash: 4});
-    tl.to("#ball", duration, {y:550, ease: "myBounce"})
-
-    //.to("#ball", {duration: 0.6, y:70, ease:"circ.in", repeat:-1, yoyo:true})
     return tl; 
 }
-function dot (){
+
+function hatbotttom(){
     const tl=gsap.timeline();
-   
+    tl.fromTo("#stroke-1", {drawSVG:"50% 50%"}, {duration: 1, drawSVG:"0% 100%"}, "sametime");
+    tl.fromTo("#stroke-2", {drawSVG:"50% 50%"}, {duration: 1, drawSVG:"0% 100%"}, "sametime");
+    tl.to("#bottom-of-hat", {opacity: 1, duration: .30})
+
+
     return tl; 
 }
 
-function morphteacup(){
-    
-}
 
 mainTL.add(ball())
-mainTL.add(dot())
-mainTL.add(morphteacup())
+mainTL.add(hatbotttom())
+
 
     
 GSDevTools.create();
+
+
